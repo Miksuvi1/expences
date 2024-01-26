a0cd4362ef76617cba141551fc6dcc69fe026471
Maiki <maiki.suvi@voco.ee>
2024-01-26 13:40:34 +0200

    expenses

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,9 +1,13 @@
+import ExpenseItem from './ExpenseItem'
 import './Expenses.css'
+import Card from './Card'
 function Expenses(props) {
     return (
-        <div className='expenses'>
-
-
-        </div>
+        <Card className='expenses'>
+            <ExpenseItem expenseData={props.expenses[0]} />
+            <ExpenseItem expenseData={props.expenses[1]} />
+        </Card>
     )
 }
+
+export default Expenses
