e2067f1a5962e3611295b336d3bf00de7f4fb970
Maiki <maiki.suvi@voco.ee>
2024-01-30 12:44:51 +0200

    YL 10

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,58 +1,33 @@
 import './ExpenseItem.css'
+import Card from '../UI/Card'
 import ExpenseDate from './ExpenseDate'
-/*function ExpenseItem() {
-    return (
-        <div className="expense-item">
-            <div>Date</div>
-            <div className="expense-item_description">
-                <h2>Title</h2>
-                <div className="expense-item_price">Amount</div>
-            </div>
-        </div>
-    )
-}
-function ExpenseItem() {
-    const date = new Date(2023, 9, 6)
-    const title = 'New book'
-    const price = 30.99
-
-    return (
-        <div className="expense-item">
-            <div>{date.toString()}</div>
-            <div>
-                <h2>{title}</h2>
-                <div>{price}</div>
-            </div>
-        </div>
-    )
-}
-
-function ExpenseItem(props) {
-    console.log(props)
-
-    return (
-        <div className="expense-item">
-        <div>{props.expenseData.date.toString()}</div>
-        <div>
-            <h2>{props.expenseData.title}</h2>
-            <div>{props.expenseData.price}</div>
-        </div>
-        </div>
-
-
-    )
-}*/
+import { useState } from 'react'
 
 const ExpenseItem = (props) => {
+    const [title, setTitle] = useState(props.expenseData.title)
+
+    const clickHandler = () => {
+        console.log('Clicked!')
+    setTitle (`Updated by click ${title}`)
+    console.log(title)
+    }
+    
   
    return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
     <ExpenseDate date = {props.expenseData.date}/>
     <div className='expense-item__description'>
     <h2>{props.expenseData.price}</h2>
     <div className='expense-item__price'>{props.expenseData.price}</div>
 </div>
-</div>
+<button onClick={clickHandler}>Click Me</button>
+</Card>
    )
 }
+
 export default ExpenseItem
+
+
+    
+
+
