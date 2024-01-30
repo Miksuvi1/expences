b9b99f657ed144bcb1db6b718338a87a35e1e4a7
Maiki <maiki.suvi@voco.ee>
2024-01-30 13:57:56 +0200

    YL11.1

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,23 @@
+import './ExpenseForm.css'
 const ExpenseForm = (props) => {
     return(
         <form>
-            <div>
-                <div>
-                    <label>Title</label>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label className="new-expense__control">Title</label>
                     <input type="text"/>
                 </div>
                 <div>
-                    <label>Amount</label>
+                    <label className="new-expense__control">Amount</label>
                     <input type="number" min="0.01" step="0.01"/>
                 </div>
                 <div>
-                    <label>date</label>
+                    <label className="new-expense__control">date</label>
                     <input type="date" min="2023-01-01" max="2025-01-31"/>
                 </div>
                 
             </div>
-                <div>
+                <div className='new-expense__actions'>
                     <button type="submit">Add Expense</button>
                 </div>
         </form>
