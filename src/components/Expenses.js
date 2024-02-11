d6edd7019c2600200cf52ffa6245da632da3a19c
Maiki <maiki.suvi@voco.ee>
2024-02-11 20:42:04 +0200

    PV

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,11 +1,13 @@
 import ExpenseItem from './ExpenseItem'
 import './Expenses.css'
 import Card from '../UI/Card'
+import ExpensesFilter from './ExpensesFilter'
 const Expenses = (props) => {
     return (
         <Card className='expenses'>
             <ExpenseItem expenseData={props.expenses[0]} />
             <ExpenseItem expenseData={props.expenses[1]} />
+            
         </Card>
     )
 }
