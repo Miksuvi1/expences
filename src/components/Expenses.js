2630160651af9a9edf50333c03771078d5a65b75
Maiki <maiki.suvi@voco.ee>
2024-02-13 13:38:27 +0200

    YL12.1

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -3,11 +3,13 @@ import './Expenses.css'
 import Card from '../UI/Card'
 import ExpensesFilter from './ExpensesFilter'
 const Expenses = (props) => {
+    const filterChangeHandler = (filteredYear) => {
+        console.log('Year log in Expenses.js' + filteredYear)
+    }
     return (
         <Card className='expenses'>
-            <ExpensesFilter >
+            <ExpensesFilter onChangeFilter={filterChangeHandler}/>
             
-            </ExpensesFilter>
             <ExpenseItem expenseData={props.expenses[0]} />
             <ExpenseItem expenseData={props.expenses[1]} />
             
